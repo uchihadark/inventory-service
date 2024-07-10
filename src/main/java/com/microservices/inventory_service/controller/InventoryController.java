1b7f907edca3adddef645c6c5d6ad98b699d4f11
uchihadark <anime.singh8950@gmail.com>
2024-07-11 01:01:33 +0530

    updated docker

--- src/main/java/com/microservices/inventory_service/controller/InventoryController.java
+++ src/main/java/com/microservices/inventory_service/controller/InventoryController.java
@@ -2,12 +2,14 @@ package com.microservices.inventory_service.controller;
 
 import com.microservices.inventory_service.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/inventory")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryController {
 
     private final InventoryService inventoryService;
@@ -15,6 +17,7 @@ public class InventoryController {
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity){
+        log.info("Received stocks check request for skuCode : {}", skuCode);
         return inventoryService.isInStock(skuCode,quantity);
     }
 }
